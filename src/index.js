37867011a777bfef4c14ac0d8b046fd04b9e53d4
KurokoTakaakiGitHub <66106869+KurokoTakaakiGitHub@users.noreply.github.com>
2022-02-26 14:54:19 +0900

    Function to add a task (character string)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // 値取得、初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成、クラス名追加
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li生成、入力値を設定する
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divの子要素にliを追加
+  div.appendChild(li);
+
+  // listの子要素にdivを追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
